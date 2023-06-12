5637f387b161fcadd0ce7f81f756d15b674fcc52
jesuswav <garciajesse2003@gmail.com>
2023-06-11 19:30:03 -0600

    media querys de login registro y principal

---
+++ src/scripts.js
@@ -0,0 +1,18 @@
+const aside = document.querySelector(".aside");
+const button = document.querySelector("#open-aside");
+const icon = document.querySelector(".arrow-icon");
+
+function visible() {
+  let clase = aside.classList;
+  console.log(clase);
+  if (aside.classList[1] === "hidden") {
+    aside.classList.remove("hidden");
+    aside.classList.add("visible");
+    icon.classList.add("arrow-icon-rotate");
+  } else {
+    aside.classList.remove("visible");
+    aside.classList.add("hidden");
+    icon.classList.remove("arrow-icon-rotate");
+  }
+}
+button.addEventListener("click", visible);
